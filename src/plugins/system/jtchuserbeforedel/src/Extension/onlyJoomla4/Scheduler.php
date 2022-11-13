cfb55e593b5605b8b300a8e4a8a39176cbbb1b3f
Guido De Gobbis <10517822+degobbis@users.noreply.github.com>
2022-11-13 08:26:31 +0100

    Add - Joomla 4 core extension missed
    - Workflows
    - Scheduler

---
+++ src/plugins/system/jtchuserbeforedel/src/Extension/onlyJoomla4/Scheduler.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  System.Jtchuserbeforedel
+ *
+ * @author      Guido De Gobbis <support@joomtools.de>
+ * @copyright   Copyright JoomTools.de - All rights reserved.
+ * @license     GNU General Public License version 3 or later
+ */
+
+namespace JtChUserBeforeDel\Extension;
+
+defined('_JEXEC') or die;
+
+use JtChUserBeforeDel\JtChUserBeforeDelInterface;
+
+/**
+ * Class to support the core extension 'com_content'.
+ *
+ * @since  1.0.1
+ */
+class Scheduler implements JtChUserBeforeDelInterface
+{
+    /**
+     * The extensions real name language string.
+     *
+     * @return  string
+     *
+     * @since   1.0.1
+     */
+    public function getExtensionRealNameLanguageString()
+    {
+        return $this->getExtensionBaseContext();
+    }
+
+    /**
+     * The extensions first/base part of the context.
+     *
+     * @return  string
+     *
+     * @since   1.0.1
+     */
+    public function getExtensionBaseContext()
+    {
+        return 'com_scheduler';
+    }
+
+    /**
+     * The database table and columns about the user information to change.
+     *
+     * @return  array
+     *
+     * @since   1.0.1
+     * @see     JtChUserBeforeDelInterface
+     */
+    public function getColumsToChange()
+    {
+        return array(
+            array(
+                'tableName' => '#__scheduler_tasks',
+                'uniqueId'  => 'id',
+                'author'    => 'created_by',
+            ),
+        );
+    }
+}
